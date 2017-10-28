3c0e1e8480f7f0fa2aa9cd23c8d2982b055f8494
sm-c <33117272+sm-c@users.noreply.github.com>
2017-10-28 07:29:03 +0300

    Create ownable.sol

---
+++ ownable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.16;
+contract Ownable {
+    address owner;
+    
+    function Ownable() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+    
+}
