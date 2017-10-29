d13f3d11b6a36c6e795bc69dacea508b8d41e696
sm-c <33117272+sm-c@users.noreply.github.com>
2017-10-29 09:25:26 +0300

    Create AbstractA.sol

---
+++ AbstractA.sol
@@ -0,0 +1,4 @@
+contract AbstractA {
+   function f1(bool arg1, uint arg2) returns(uint); 
+   // No implementation, just the function signature. This is just so Solidity can work out how to call it.
+}
